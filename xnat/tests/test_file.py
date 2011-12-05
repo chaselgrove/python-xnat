4d88f5f2d2ae225df9cd653d036382f8d5fe6cf5
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2011-12-05 08:53:29 -0500

    file and scan resource tests

---
+++ xnat/tests/test_file.py
@@ -0,0 +1,32 @@
+import pyxnat.core.resources
+from .. import Connection
+
+def setup():
+    global connection, project, subject, experiment, scan, resource, file
+    connection = Connection('https://central.xnat.org', 
+                            'nosetests', 
+                            'nosetests')
+    project = connection.projects['PALS']
+    subject = project.subjects['Human_Buckner_Case01']
+    experiment = subject.experiments['Human_Buckner_Case01']
+    scan = experiment.scans['mpr-1']
+    resource = scan.resources['ANALYZE']
+    file = resource.files['OAS1_0054_MR1_mpr-1_anon.hdr']
+
+def test_attributes():
+    assert file.connection is connection
+    assert file.resource is resource
+    assert isinstance(file.pyxnat_file, pyxnat.core.resources.File)
+    assert file.path == 'OAS1_0054_MR1_mpr-1_anon.hdr'
+    assert file.size == 348
+    assert file.last_modified == 'Thu, 13 Jul 2006 14:20:07 GMT'
+
+def test_data():
+    data = file.read()
+    assert isinstance(data, str)
+    assert len(data) == 348
+
+def teardown():
+    connection.close()
+
+# eof
---
+++ xnat/tests/test_scan_resource.py
@@ -0,0 +1,34 @@
+import pyxnat.core.resources
+from .. import Connection, _File
+
+def setup():
+    global connection, project, subject, experiment, scan, resource
+    connection = Connection('https://central.xnat.org', 
+                            'nosetests', 
+                            'nosetests')
+    project = connection.projects['PALS']
+    subject = project.subjects['Human_Buckner_Case01']
+    experiment = subject.experiments['Human_Buckner_Case01']
+    scan = experiment.scans['mpr-1']
+    resource = scan.resources['ANALYZE']
+
+def test_attributes():
+    assert resource.connection is connection
+    assert resource.project is project
+    assert resource.subject is subject
+    assert resource.experiment is experiment
+    assert resource.scan is scan
+    assert resource.id == 1408
+    assert resource.label == 'ANALYZE'
+
+def test_files():
+    assert isinstance(resource.files, dict)
+    assert 'OAS1_0054_MR1_mpr-1_anon.img' in resource.files
+    assert isinstance(resource.files['OAS1_0054_MR1_mpr-1_anon.img'], _File)
+    assert 'xxxxxxxx' not in resource.files
+    assert len(resource.files) == 3
+
+def teardown():
+    connection.close()
+
+# eof
