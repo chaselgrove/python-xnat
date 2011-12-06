bf8359c3d4363b6e142435c0ae382187d20f5a1f
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2011-12-06 07:33:29 -0500

    reconstruction/assessment order

--- xnat/tests/test_resource_dicts.py
+++ xnat/tests/test_resource_dicts.py
@@ -9,8 +9,8 @@ def setup():
     subject = c.projects['nosetests'].subjects['resource_test']
     experiment = c.projects['nosetests'].subjects['resource_test'].experiments['resource_test_MR']
     scan = c.projects['PALS'].subjects['Human_Buckner_Case01'].experiments['Human_Buckner_Case01'].scans['mpr-1']
-    assessment = c.projects['nosetests'].subjects['resource_test'].experiments['resource_test_MR'].assessments['assessment']
     reconstruction = c.projects['nosetests'].subjects['resource_test'].experiments['resource_test_MR'].reconstructions['recon']
+    assessment = c.projects['nosetests'].subjects['resource_test'].experiments['resource_test_MR'].assessments['assessment']
 
 def test_project_resources():
     assert isinstance(project.resources, dict)
@@ -36,16 +36,6 @@ def test_scan_resources():
     assert isinstance(scan.resources['ANALYZE'], _ScanResource)
     assert len(scan.resources) == 1
 
-def test_assessment_resources():
-    assert isinstance(assessment.in_resources, dict)
-    assert isinstance(assessment.out_resources, dict)
-    assert 'tia' in assessment.in_resources
-    assert 'toa' in assessment.out_resources
-    assert isinstance(assessment.in_resources['tia'], _AssessmentResource)
-    assert isinstance(assessment.out_resources['toa'], _AssessmentResource)
-    assert len(assessment.in_resources) == 1
-    assert len(assessment.out_resources) == 1
-
 def test_reconstruction_resources():
     assert isinstance(reconstruction.in_resources, dict)
     assert isinstance(reconstruction.out_resources, dict)
@@ -55,6 +45,16 @@ def test_reconstruction_resources():
     assert isinstance(reconstruction.out_resources['tor'], _ReconstructionResource)
     assert len(reconstruction.in_resources) == 1
     assert len(reconstruction.out_resources) == 1
+
+def test_assessment_resources():
+    assert isinstance(assessment.in_resources, dict)
+    assert isinstance(assessment.out_resources, dict)
+    assert 'tia' in assessment.in_resources
+    assert 'toa' in assessment.out_resources
+    assert isinstance(assessment.in_resources['tia'], _AssessmentResource)
+    assert isinstance(assessment.out_resources['toa'], _AssessmentResource)
+    assert len(assessment.in_resources) == 1
+    assert len(assessment.out_resources) == 1
 
 def teardown():
     c.close()
