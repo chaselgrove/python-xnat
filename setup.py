8bcb53a8924615b1bba2c84eb708ad5e17476b00
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2011-12-31 16:33:39 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='xnat', 
+      version='0.1', 
+      description='Abstraction layer for pyxnat', 
+      author='Christian Haselgrove', 
+      author_email='christian.haselgrove@umassmed.edu', 
+      packages=['xnat'])
+
+# eof
